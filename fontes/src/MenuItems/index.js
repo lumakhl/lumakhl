1d98e9682241bdcc291314f7b85c773efc26effd
AlexSerodio <alequis01@gmail.com>
2020-04-30 18:40:01 -0300

    Alterado dados da lista de itens

--- fontes/src/MenuItems/index.js
+++ fontes/src/MenuItems/index.js
@@ -6,28 +6,32 @@ import './styles.scss';
 
 const items = [
     {
+        id: 0,
         name: "Escova",
-        price: "5,99",
-        quantity: "20",
-        image: mouthwashImage
+        image: mouthwashImage,
+        price: 5.99,
+        quantity: 20
     },
     {
+        id: 1,
         name: "Enxaguante",
-        price: "15,99",
-        quantity: "7",
-        image: mouthwashImage
+        image: mouthwashImage,
+        price: 15.99,
+        quantity: 7
     },
     {
+        id: 2,
         name: "Escova Elétrica",
-        price: "49,99",
-        quantity: "20",
-        image: mouthwashImage
+        image: mouthwashImage,
+        price: 49.99,
+        quantity: 20
     },
     {
+        id: 3,
         name: "Enxaguante Bucal",
-        price: "15,99",
-        quantity: "201",
-        image: mouthwashImage
+        image: mouthwashImage,
+        price: 15.99,
+        quantity: 201
     }
 ];
 
@@ -38,7 +42,8 @@ export default function MenuItems() {
                 <h2>Loja de itens</h2>
 
                 {items.map(item => (
-                    <Item name={item.name} price={item.price} quantity={item.quantity} image={item.image} />
+                    <Item key={item.id} name={item.name} image={item.image}  
+                        price={item.price} quantity={item.quantity} />
                 ))}
             </section>
         </div>

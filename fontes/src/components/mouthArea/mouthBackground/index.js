f3d81c5dcebf953ec038ca6651cf2e25b1d71526
Adriner Andrade <adriner_andrade@hotmail.com>
2020-05-21 20:20:10 -0300

    animação do fundo

---
+++ fontes/src/components/mouthArea/mouthBackground/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './styles.scss';
+
+export default class MouthBackground extends React.Component {
+
+  render() {
+    return (
+    <div className="mouth-view-background">
+      <div></div>
+      <div></div>
+    </div>
+    )
+  }
+
+}

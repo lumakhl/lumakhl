7996101746edf2cf51e44949702e9b01dcf15238
Adriner Andrade <adriner_andrade@hotmail.com>
2020-05-21 22:02:01 -0300

    fazendo a animação dos itens ser aleatória

--- fontes/src/components/inventory/InventoryItem/index.js
+++ fontes/src/components/inventory/InventoryItem/index.js
@@ -10,9 +10,10 @@ export default function InventoryItem(props) {
 
     return (
         <Tile>
-            <img className="inventory-item-image" src={image} alt={name} />
-            <img className="inventory-item-image" src={image} alt={name} />
-            <img className="inventory-item-image" src={image} alt={name} />
+            <img className="inventory-item-image animation-1" src={image} alt={name} />
+            <img className="inventory-item-image animation-2" src={image} alt={name} />
+            <img className="inventory-item-image animation-3" src={image} alt={name} />
+            <img className="inventory-item-image animation-4" src={image} alt={name} />
         </Tile>
     );
 }
